cd0d7b93ab675b6acaf542cf6b70b1146a38303d
SamoeiLinda <you@example.com>
2022-07-13 08:26:41 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
- *  * _printf - produces output accordin
+ *  * _printf - pritns out a formatted string
+ * @format: the string to be formatted
+ * Return: the length of the string
+ */
+
+int _printf(const char *format, ...)
+{
+	int len;
+
+	va_list list;
+	va_start(list, format);
+	len = vfprintf(stdout, format, list);
+	va_end(list);
+
+	return (len);
+}
