567aaf130ef0d4c9927734a02a1d69eb791dbe3d
SamoeiLinda <you@example.com>
2022-07-13 12:11:00 -0700

    main_0.c

---
+++ main_0.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *  * main - Entry point
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+	int len, len2;
+	char x[50], y[50]
+
+	printf("Let's print a simple sentence.\n");
+	scanf("%c", &x);
+	printf("Let's print a simple sentence.\n");
+	scanf("%c", &y);
+	len = strlen(x);
+	len2 = strlen(y);
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
+}
