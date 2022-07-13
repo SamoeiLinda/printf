005d4af997b7c955e1a1818a866d186ace818ed7
SamoeiLinda <you@example.com>
2022-07-13 08:30:33 -0700

    remove _printf.c.swp

--- _printf.c
+++ _printf.c
@@ -1,19 +1,4 @@
 #include "main.h"
 
 /**
- *  * _printf - pritns out a formatted string
- * @format: the string to be formatted
- * Return: the length of the string
- */
-
-int _printf(const char *format, ...)
-{
-	int len;
-
-	va_list list;
-	va_start(list, format);
-	len = vfprintf(stdout, format, list);
-	va_end(list);
-
-	return (len);
-}
+ *  * _printf - produces output accordin
